669ff4017330438cec03ea2a0c461b92c6f49317
kelvynkhrystian <khrystian.dev@hotmail.com>
2026-03-20 18:31:37 -0300

    refatorando server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 import app from './src/app.js'
 
+const PORT = process.env.PORT || 3000
 
-const PORT = 3000
-
-// subir servidor
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+  console.log(`Servidor rodando na porta ${PORT} 🚀`)
+})

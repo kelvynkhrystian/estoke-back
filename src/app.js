38c199a97339897a843fcb70f3c1f21c7d0287ba
kelvynkhrystian <khrystian.dev@hotmail.com>
2026-03-21 08:37:12 -0300

    test7

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import pool from './config/database.js'
+// import pool from './config/database.js'
 
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'

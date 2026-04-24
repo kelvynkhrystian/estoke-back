3005204b114ad804f0da2928561dd1ce3211e3fe
kelvynkhrystian <khrystian.dev@hotmail.com>
2026-04-24 19:05:05 -0300

    removendo codigo antigo de controller

--- src/controllers/configController.js
+++ src/controllers/configController.js
@@ -17,13 +17,3 @@ export const update = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
-
-export const getConfig = async (req, res) => {
-  try {
-    const [rows] = await pool.query('SELECT * FROM app_config LIMIT 1');
-    return res.json(rows[0] || {});
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: 'Erro ao buscar configurações' });
-  }
-};
